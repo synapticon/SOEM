a5170c1ad3e41e737ddfb8f5070fffeffcfe8ea5
Marko Sanković <msankovic@synapticon.com>
2024-07-29 21:33:20 +0200

    Rework FoE busy handling
    See:
    - https://github.com/OpenEtherCATsociety/SOEM/pull/627
    - https://github.com/OpenEtherCATsociety/SOEM/pull/623

--- soem/ethercatbase.c
+++ soem/ethercatbase.c
@@ -325,7 +325,15 @@ int ecx_FPRD(ecx_portt *port, uint16 ADP, uint16 ADO, uint16 length, void *data,
    wkc = ecx_srconfirm(port, idx, timeout);
    if (wkc > 0)
    {
-      memcpy(data, &(port->rxbuf[idx][EC_HEADERSIZE]), length);
+      // https://github.com/OpenEtherCATsociety/SOEM/pull/623/commits/ebbe5da29d969ed0847a1c312d20e8ab345e8859
+      if (length <= EC_MAXMBX)
+      {
+         memcpy(data, &(port->rxbuf[idx][EC_HEADERSIZE]), length);
+      }
+      else
+      {
+         wkc = EC_ERROR;
+      }
    }
    ecx_setbufstat(port, idx, EC_BUF_EMPTY);
 
